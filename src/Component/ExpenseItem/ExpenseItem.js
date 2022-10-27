c9fcc0a577917b23b2c549776618cc703fd5eef7
Moinul Islam <moinulilm10@gmail.com>
2022-10-27 23:53:42 +0600

    03.19_concept of composition_children and props

--- src/Component/ExpenseItem/ExpenseItem.js
+++ src/Component/ExpenseItem/ExpenseItem.js
@@ -1,17 +1,18 @@
 import React from "react";
+import Card from "../Card/Card";
 import ExpensesDate from "../ExpensesDate/ExpensesDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpensesDate date={props.date} />
       {/* <div>{props.date.toISOString()}</div> */}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
